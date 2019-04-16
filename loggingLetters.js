7a7cc2423f3844994109d5d826e7f780694975b2
mariedouyon305 <mariedouyon305@gmail.com>
2019-04-16 15:57:19 -0400

    logging letters

--- loggingLetters.js
+++ loggingLetters.js
@@ -1,6 +1,14 @@
-var sentence = "I love learning software development";
-for (var i = 0; i < sentence.length;i++) {
+/*var sentence= "I love learning software development";
+
+for (var i = 0; i < sentence.length; i++) {
 	if(sentence[i]!== ""){
 		console.log(sentence[i]);
 	}
+}*/
+var n = 25;
+var sum = 0;
+
+for (sum = 0; sum <= n; sum + 2) {
+
+	console.log(sum)
 }
