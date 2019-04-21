eedddf0d8d4d04d3f46bbf4de60f1006b6b57e7d
mariedouyon305 <mariedouyon305@gmail.com>
2019-04-21 13:49:29 -0400

    click to add items testing

--- clickToAddItems.js
+++ clickToAddItems.js
@@ -11,3 +11,12 @@ function cleaerrors() {
     }
   }
 }
+
+function resetForms() {
+  clearErrors();
+  document.forms["numberFun"]["num1"].value = "";
+  document.forms["numberFun"]["num2"].value = "";
+  document.getElementById("results").style.display = "none"
+    document.getElementById("submitButton").innerText = "Submit";
+    document.forms["numberFun"]["num1"].focus();
+}
