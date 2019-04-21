7f7a0603c6008d27d76bde9961d467b0ebe3084d
mariedouyon305 <mariedouyon305@gmail.com>
2019-04-21 07:33:54 -0400

    currentElements

---
+++ comparingElements.js
@@ -0,0 +1,23 @@
+var testArray = [0,1,1,"1",3,"311"];
+
+for (var arrayPosition = 0; arrayPosition <testArray.length -1; arrayPosition++){
+  /* conditon states that arrayPosition is less than the length of the array and
+  substract 1 from the length **move on to the next and add 1 */
+
+  var currentElement = testArray[arrayPosition];
+  //  new var created once the array position has been made
+
+  var nextElement = testArray[arrayPosition + 1];
+  // 2nd var does the same as currentElement but it adds 2
+
+  console.log ("Testing " + currentElement + " and " + nextElement + 
+  "(greater than): " + (currentElement > nextElement));
+
+  if (currentElement !== nextElement){
+    
+     console.log("Testing " + currentElement + " and " + nextElement + "(strictly equal to): " + 
+  (currentElement === nextElement))
+  }
+
+
+}
