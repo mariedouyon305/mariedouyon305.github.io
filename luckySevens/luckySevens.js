76646d41b00636dac20dea93e3276719706333e6
mariedouyon305 <mariedouyon305@gmail.com>
2019-04-22 17:35:59 -0400

    lucky testing

---
+++ luckySevens/luckySevens.js
@@ -0,0 +1,52 @@
+function rollDice(){
+
+	console.log("inside roll dice function");
+	
+	var money = Number(document.getElementById("firstBet").value)
+
+	while (money > 0){
+
+			var roll1= Math.floor(Math.random()*6);
+			var roll2 = Math.floor(Math.random()*6);
+			var rollResults = roll1 + roll2;
+		/* keeping roll1 and roll2 inside while loop keep dice chnaging*/
+
+			// console.log("Die #1:", roll1);
+			// console.log("Die #2:", roll2);
+			// tests my dice
+			
+			if (rollResults == 7){
+			money = money + 4;
+
+			} 
+
+			else {
+
+				money -= 1;
+				// console.log("here in the while loop for rollDice");
+			}
+
+		}
+
+
+}  
+// the roll dice portion of the game
+
+function letsPlay(){
+
+	console.log("Here in letsPlay")
+	var initialBet = Number(document.getElementById("firstBet").value)
+	if (initialBet <= 0 ){
+
+		// console.log("Here in intialBet")
+		alert("Please put amount greater than 0")
+	} else if (initialBet >= 0){ 
+		console.log("bet looks good");
+		rollDice();
+
+	} else {
+		
+	}
+		 
+}
+
