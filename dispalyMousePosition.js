3b890b2838dacaa99c4220660bb5ddf03c7cbdc6
mariedouyon305 <mariedouyon305@gmail.com>
2019-04-21 22:24:25 -0400

    display mouse position testing

--- dispalyMousePosition.js
+++ dispalyMousePosition.js
@@ -1,11 +1,12 @@
 var isMouseTracking = false;
  
 // We need a function to update the position
-// On our front end, we have 2 labels - 
 // one for X, one for Y
+
 function updateMousePosition() {
     // If tracking is enabled, update the labels
     if (isMouseTracking) {
+
         // We can get the mouse positions with
         // event.clientX and event.clientY.
         // We can update the text of an HTML element with the
@@ -19,7 +20,8 @@ function updateMousePosition() {
  
 // We need a function to toggle whether tracking is enabled
 function toggleTracking() {
-    // If isMouseTracking is true, set it to false
+    // If isMouseTracking is true, se
+    t it to false
     // Otherwise it is false, and needs to be set to true
     isMouseTracking = !isMouseTracking;
     // Update the tracking status to show whether it is enabled
