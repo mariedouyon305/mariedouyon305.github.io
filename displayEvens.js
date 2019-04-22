2f10342c239f45b1ab1d6fef5351fffe77518c9a
mariedouyon305 <mariedouyon305@gmail.com>
2019-04-22 13:41:34 -0400

    displaying evens testing

--- displayEvens.js
+++ displayEvens.js
@@ -1,21 +1,29 @@
-// var 
 
 function displayEvens(){
 	// console.log("Here in displayEvens");
-	var num1 = document.getElementById("startNum").value;
-	var num2 = document.getElementById("endNum").value;
-	var step = document.getElementById("increments").value;
+	var num1 = Number(document.getElementById("startNum").value);
+	var num2 = Number(document.getElementById("endNum").value);
+	var step = Number(document.getElementById("increments").value);
 
+	document.getElementById("message").innerText = "Here are the even numbers between "+num1+ " and "+num2+
+	" by "+step+"'s:";
+	for (var i = num1; i <= num2; i += step){
+		
+		if(i % 2 == 0){
+		 	console.log(i);
+		 	document.getElementById("results").innerText += i+"\n";
+		}
 
-	for (var i = num1; i < num2; i += step);{
-		console.log(i);
-		 // if(i % 2 == 0){
-		 // 	console.log(i);
-		 // }
+		if(isNaN(num1) || isNaN(num2) || isNaN(step)){
+			alert("please only input numbers");
+		} 
 
-		 
-		 	
+		if(num1 < 0 || num2 < 0 || step < 0 ){
+			alert("Please input numbers above 0");
+		}
+
+		if(num2 <= num1){
+			alert("your Ending number cannot be less than or eqaul to Starting number Tray again");
+		}
 	}
-	// document.getElementById("startNum") 
-	//console.log(i);
 }
