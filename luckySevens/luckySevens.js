cfa1783d643f1726ca896ec6c07bfdc245753798
mariedouyon305 <mariedouyon305@gmail.com>
2019-04-23 14:34:47 -0400

    added bootstrap -CDN - restaurant 3 Project and added code for luckySevens

--- luckySevens/luckySevens.js
+++ luckySevens/luckySevens.js
@@ -1,13 +1,16 @@
 function rollDice(){
 
-	console.log("inside roll dice function");
+	// console.log("inside roll dice function");
 	
-	var money = Number(document.getElementById("firstBet").value)
+	var money = Number(document.getElementById("firstBet").value);
+	var maxMoney = 0; 
+	// you dont want to place this variable inside the while  
+	//  you need it to chang to the money var 
 
 	while (money > 0){
 
-			var roll1= Math.floor(Math.random()*6);
-			var roll2 = Math.floor(Math.random()*6);
+			var roll1= Math.ceil(Math.random()*6);
+			var roll2 = Math.ceil(Math.random()*6);
 			var rollResults = roll1 + roll2;
 		/* keeping roll1 and roll2 inside while loop keep dice changing*/
 
@@ -25,23 +28,30 @@ function rollDice(){
 				money -= 1;
 				// console.log("here in the while loop for rollDice");
 			}
-
+			// console.log(rollResults) 
+			// what is the sum of roll1 and roll 2-- track of rolls before losing
+			// console.log(money)
+			// what were the earning each turn --win or lost
+	if ( money > maxMoney ){
+		maxMoney = money;
+	 console.log(maxMoney);
+	 //  outputs that max amount of money until the game is lost
+	}
 		}
-
 
 }  
 // the roll dice portion of the game
 
 function letsPlay(){
 
-	console.log("Here in letsPlay")
+	// console.log("Here in letsPlay")
 	var initialBet = Number(document.getElementById("firstBet").value)
 	if (initialBet <= 0 ){
 
 		// console.log("Here in intialBet")
 		alert("Please put amount greater than 0")
 	} else if (initialBet >= 0){ 
-		console.log("bet looks good");
+		// console.log("bet looks good");
 		rollDice();
 
 	} else {
